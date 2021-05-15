017531edef78c3b1919e8525852e2ce26ae87a1f
Sahil Kavitake <sahir.kavitake202@gmail.com>
2021-05-15 20:58:51 +0530

    Update Solution.py

--- Solution.py
+++ Solution.py
@@ -2,12 +2,11 @@
 
 def validPos(piece , currentpos , nextpos):
     letters = ['a','b','c','d','e','f','g','h']
-    numbers = [1,2,3,4,5,6,7,8]
     
     currLetIndex = letters.index(currentpos[0])
     nextLetindex = letters.index(nextpos[0])
-    currNumIndex = numbers.index(int(currentpos[1]))
-    nextNumindex = numbers.index(int(nextpos[1]))
+    currNumIndex = int(currentpos[1])
+    nextNumindex = int(nextpos[1])
     
     if(piece == "Bishop"):
         if(abs(currLetIndex - nextLetindex) == abs(currNumIndex - nextNumindex)):
